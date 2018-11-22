5fae6bcb9520ae10cc1385da342130f8dae00bad
gdynusa <jana.horecka@cognexa.com>
2018-11-22 14:58:41 +0100

    Add tests

--- tests/test_aiohttp_error_handlers.py
+++ tests/test_aiohttp_error_handlers.py
@@ -9,17 +9,39 @@ def app_with_errors(aiohttp_apistrap):
     app = web.Application(debug=True)
     routes = web.RouteTableDef()
 
-    @routes.get("/client_error")
+    @routes.get('/client_error')
     def view_1(request):
         raise ApiClientError()
 
-    @routes.get("/server_error")
+    @routes.get('/server_error')
     def view_2(request):
         raise ApiServerError()
 
-    @routes.get("/internal_error")
+    @routes.get('/internal_error')
     def view_3(request):
-        raise RuntimeError("Runtime error occurred")
+        raise RuntimeError('Runtime error occurred')
+
+    app.add_routes(routes)
+    aiohttp_apistrap.init_app(app)
+    yield app
+
+
+@pytest.fixture()
+def app_with_errors_in_production(aiohttp_apistrap):
+    app = web.Application(debug=False)
+    routes = web.RouteTableDef()
+
+    @routes.get('/client_error')
+    def view_1(request):
+        raise ApiClientError()
+
+    @routes.get('/server_error')
+    def view_2(request):
+        raise ApiServerError()
+
+    @routes.get('/internal_error')
+    def view_3(request):
+        raise RuntimeError('Runtime error occurred')
 
     app.add_routes(routes)
     aiohttp_apistrap.init_app(app)
@@ -28,7 +50,15 @@ def app_with_errors(aiohttp_apistrap):
 
 async def test_http_error_handler(app_with_errors, aiohttp_client):
     client = await aiohttp_client(app_with_errors)
-    response = await client.get("/nonexistent_url")
+    response = await client.get('/nonexistent_url')
+    assert response.status == 404
+    data = await response.json()
+    assert 'debug_data' not in data
+
+
+async def test_http_error_handler_in_production(app_with_errors_in_production, aiohttp_client):
+    client = await aiohttp_client(app_with_errors_in_production)
+    response = await client.get('/nonexistent_url')
     assert response.status == 404
     data = await response.json()
     assert 'debug_data' not in data
@@ -36,27 +66,51 @@ async def test_http_error_handler(app_with_errors, aiohttp_client):
 
 async def test_client_error_handler(app_with_errors, aiohttp_client):
     client = await aiohttp_client(app_with_errors)
-    response = await client.get("/client_error")
+    response = await client.get('/client_error')
     assert response.status == 400
     data = await response.json()
     assert 'debug_data' in data
     assert data['debug_data']['exception_type'] == 'ApiClientError'
 
 
+async def test_client_error_handler_in_production(app_with_errors_in_production, aiohttp_client):
+    client = await aiohttp_client(app_with_errors_in_production)
+    response = await client.get('/client_error')
+    assert response.status == 400
+    data = await response.json()
+    assert 'debug_data' not in data
+
+
 async def test_server_error_handler(app_with_errors, aiohttp_client):
     client = await aiohttp_client(app_with_errors)
-    response = await client.get("/server_error")
+    response = await client.get('/server_error')
     assert response.status == 500
     data = await response.json()
     assert 'debug_data' in data
     assert data['debug_data']['exception_type'] == 'ApiServerError'
 
 
-async def test_internal_server_error_handler(app_with_errors, aiohttp_client):
-    client = await aiohttp_client(app_with_errors)
-    response = await client.get("/internal_error")
+async def test_server_error_handler_in_production(app_with_errors_in_production, aiohttp_client):
+    client = await aiohttp_client(app_with_errors_in_production)
+    response = await client.get('/server_error')
     assert response.status == 500
     data = await response.json()
-    assert data["message"] == "Runtime error occurred"
+    assert 'debug_data' not in data
+
+
+async def test_internal_server_error_handler(app_with_errors, aiohttp_client):
+    client = await aiohttp_client(app_with_errors)
+    response = await client.get('/internal_error')
+    assert response.status == 500
+    data = await response.json()
+    assert data['message'] == 'Runtime error occurred'
     assert 'debug_data' in data
     assert data['debug_data']['exception_type'] == 'RuntimeError'
+
+
+async def test_internal_server_error_handler_in_production(app_with_errors_in_production, aiohttp_client):
+    client = await aiohttp_client(app_with_errors_in_production)
+    response = await client.get('/internal_error')
+    assert response.status == 500
+    data = await response.json()
+    assert data['message'] == 'Internal server error'
--- tests/test_aiohttp_responds_with_decorator.py
+++ tests/test_aiohttp_responds_with_decorator.py
@@ -3,6 +3,8 @@ from aiohttp import web
 from schematics import Model
 from schematics.types import StringType, IntType
 
+from apistrap.errors import UnexpectedResponseError, InvalidResponseError
+
 
 class OkResponse(Model):
     string_field = StringType(required=True)
@@ -10,6 +12,10 @@ class OkResponse(Model):
 
 class ErrorResponse(Model):
     error_message = StringType(required=True)
+
+
+class WeirdResponse(Model):
+    string_field = StringType(required=True)
 
 
 class RequestModel(Model):
@@ -22,15 +28,29 @@ def app_with_responds_with(aiohttp_apistrap):
     app = web.Application()
     routes = web.RouteTableDef()
 
-    @routes.get("/")
+    @routes.get('/')
     @aiohttp_apistrap.responds_with(OkResponse)
+    @aiohttp_apistrap.responds_with(ErrorResponse, code=400)
     async def view(request):
-        return OkResponse(dict(string_field="Hello World"))
+        return OkResponse(dict(string_field='Hello World'))
 
-    @routes.get("/error")
+    @routes.get('/error')
+    @aiohttp_apistrap.responds_with(OkResponse)
     @aiohttp_apistrap.responds_with(ErrorResponse, code=400)
     async def error(request):
-        return ErrorResponse(dict(error_message="Error"))
+        return ErrorResponse(dict(error_message='Error'))
+
+    @routes.get('/weird')
+    @aiohttp_apistrap.responds_with(OkResponse)
+    @aiohttp_apistrap.responds_with(ErrorResponse, code=400)
+    async def weird(request):
+        return WeirdResponse(dict(string_field='Hello Weird World'))
+
+    @routes.get('/invalid')
+    @aiohttp_apistrap.responds_with(OkResponse)
+    @aiohttp_apistrap.responds_with(ErrorResponse, code=400)
+    async def invalid(request):
+        return OkResponse(dict())
 
     app.add_routes(routes)
     yield app
@@ -38,19 +58,36 @@ def app_with_responds_with(aiohttp_apistrap):
 
 async def test_accepts(app_with_responds_with, aiohttp_client):
     client = await aiohttp_client(app_with_responds_with)
-    response = await client.get("/")
+    response = await client.get('/')
 
     assert response.status == 200
 
     data = await response.json()
-    assert data["string_field"] == "Hello World"
+    assert data['string_field'] == 'Hello World'
 
 
 async def test_error(app_with_responds_with, aiohttp_client):
     client = await aiohttp_client(app_with_responds_with)
-    response = await client.get("/error")
+    response = await client.get('/error')
 
     assert response.status == 400
 
     data = await response.json()
-    assert data["error_message"] == "Error"
+    assert data['error_message'] == 'Error'
+
+
+@pytest.mark.skip  # TODO
+async def test_weird(app_with_responds_with, aiohttp_client):
+    client = await aiohttp_client(app_with_responds_with)
+    with pytest.raises(UnexpectedResponseError):
+        await client.get('/weird')
+
+
+@pytest.mark.skip  # TODO
+async def test_invalid(app_with_responds_with, aiohttp_client):
+    client = await aiohttp_client(app_with_responds_with)
+    with pytest.raises(InvalidResponseError):
+        await client.get('/invalid')
+
+
+# TODO test file response once it's finished
