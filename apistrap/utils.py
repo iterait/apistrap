29e254b9c5e8e1942a46fa735496b64e91cf5f4d
Jan Buchar <Teyras@gmail.com>
2019-07-29 17:05:17 +0200

    Add type annotations and doc

--- apistrap/utils.py
+++ apistrap/utils.py
@@ -1,7 +1,7 @@
 import inspect
 import sys
 import traceback
-from typing import Any, Mapping
+from typing import Any, Mapping, Callable, Dict, Optional, Type, Union
 
 from more_itertools import flatten
 
@@ -38,30 +38,48 @@ def snake_to_camel(value):
     return result[0].lower() + result[1:]
 
 
-def get_function_perspective_globals(function, level=0, max_level=None):
+def get_function_perspective_globals(function: Callable) -> Dict[str, Any]:
+    """
+    Extract global variables from the stack of a function.
+
+    :param function: the function whose stack should be examined.
+    :return: a merged dictionary of globals
+    """
+
     globs = {}
-    if level != max_level:
-        stack = inspect.stack()
-        for frame in stack:
-            if frame.filename == inspect.getsourcefile(function):
-                globs.update(frame.frame.f_globals)
+    stack = inspect.stack()
+
+    for frame in stack:
+        if frame.filename == inspect.getsourcefile(function):
+            globs.update(frame.frame.f_globals)
+
     return globs
 
 
-def resolve_fw_decl(function, annotation, globs=None, level=0, search_stack_depth=2):
+def resolve_fw_decl(function: Callable, annotation: Union[None, str, Type]) -> Type:
+    """
+    Resolve a parameter annotation.
+
+    :param function: the function that owns the annotation
+    :param annotation: an annotation string
+    :return: the resolved annotation
+    """
     if annotation is None:
         raise ValueError("NoneType parameter annotations are not supported")
 
     if not isinstance(annotation, str):
         return annotation
 
-    if globs is None:
-        globs = get_function_perspective_globals(function, level + 1, level + 1 + search_stack_depth)
+    globs = get_function_perspective_globals(function)
 
     return eval(annotation, globs)
 
 
-def get_type_hints(function):
+def get_type_hints(function: Callable) -> Dict[str, Type]:
+    """
+    Get a dictionary of resolved type annotations for a function.
+    """
+
     return {
         name: resolve_fw_decl(function, annotation)
         for name, annotation in function.__annotations__.items()
