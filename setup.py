375461e25c3413ad0faa243f4a9f0e577c69f36c
Jan Buchar <Teyras@gmail.com>
2019-12-30 16:41:55 +0100

    Bump version to 0.9.8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='apistrap',
-      version='0.9.7',
+      version='0.9.8',
       description='Iterait REST API utilities',
       classifiers=[
           'Development Status :: 4 - Beta',
